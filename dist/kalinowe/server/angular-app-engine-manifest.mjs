aef8af46f7ddc7891ecde750e326a3985604b62f
pytlakow <pytlakow@gmail.com>
2025-07-11 09:12:19 +0200

    move dist to docs

--- dist/kalinowe/server/angular-app-engine-manifest.mjs
+++ dist/kalinowe/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: '/kalinowe',
   supportedLocales: {
   "en-US": ""
 },
--- dist/kalinowe/server/angular-app-manifest.mjs
+++ dist/kalinowe/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: '/kalinowe/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/"
+    "route": "/kalinowe"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 433, hash: '7c793dc2eb6c63bf1a28631e1dc02a268d5b4a84ad02ba23a7f526f7cf20f7c1', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 946, hash: '0fdeb40dc077fb6acf32c236fe47ad42c96c7cd32c973f8b857c8f2c012deaa7', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 21545, hash: '8523f03232573444ff409287cee94f19d766b347739ab41986755ee460543a58', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 442, hash: '9c5a85f98f05cb81738ee4524e5721c7338483416b90df3d802e180445bdccfb', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 955, hash: 'b1da416754567acd7fa2e6b9aabdb8b93f4dddc5f9d1d9ad0fa87a6364a38bbf', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 21554, hash: 'b10af2774e368f66fedcd85ab3f484ad19d3062e0ca9aec49cda8ea74d9d5822', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
